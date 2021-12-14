f6d2bcb135c45f0e359ebd10ec54d0dd31d54f53
joeblk74 <joeblk@gmail.com>
2021-12-14 13:41:36 -0900

    Day 2 conversion of integers to strings and floats

--- day2.py
+++ day2.py
@@ -21,7 +21,14 @@ print(123 + 345)
 True
 False
 
-num_char = len(input("what is your name?"))
-#print("Your name has" + num_char + "characters.")
+# num_char = len(input("what is your name?"))
 
-print(type(len(input("whatis your name"))))
+# new_num_char = str(num_char)
+
+# print("Your name has " + new_num_char + " characters.")
+
+a = float(123)
+print(type(a))
+
+print(70 + float("100.5"))
+print(str(70)+ str(100))
