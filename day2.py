410b35c7ec9795656aca5aa051881a33fcdd8371
joeblk74 <joeblk@gmail.com>
2021-12-19 22:31:18 -0900

    Addition of BMI calculator portion of project

--- day2.py
+++ day2.py
@@ -60,3 +60,25 @@ print(int(a) + int(b))
 
 print(3 + 3 + 3 - 3 - 3)
 print(3 * (3 + 3) / 3 - 3)
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi = int(weight) / float(height) ** 2
+bmi_as_int = int(bmi)
+print(bmi_as_int)
+
+weight_as_int = int(weight)
+height_as_float = float(height)
+
+# Using the exponent operator **
+bmi = weight_as_int / height_as_float ** 2
+# or using multiplication and PEMDAS
+bmi = weight_as_int / (height_as_float * height_as_float)
+
+bmi_as_int = int(bmi)
+
+print(bmi_as_int)
